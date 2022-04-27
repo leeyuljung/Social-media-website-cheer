1dfff86a9c59d8e220a506f651dc753c99c0473d
leeyuljung <rita810605@gmail.com>
2022-04-27 12:55:50 +0800

    modify: 檢查該 user 是否已註冊過

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,11 +1,24 @@
-const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { UserInputError } = require('apollo-server');
+const User = require('../../models/User');
 const { SECRET_KEY } = require('../../config');
 
 module.exports = {
     Mutation: {
-        async register(_, { registerInput: { username, password, passwordConfirm, email } }, context, info) {
+        async register(_, { registerInput: { username, password, passwordConfirm, email } }) {
+            
+            // 檢查 user 是否有註冊過
+            const user = await User.findOne({ username });
+
+            if(user) {
+                throw new UserInputError('Username is taken', {
+                    errors: {
+                        username: 'This username is taken'
+                    }
+                })
+            }
+
             // 將密碼加密
             password = await bcrypt.hash(password, 12);
 
