0c0719f3d6970ee2a68a597881f725ae5dd6bbe9
leeyuljung <rita810605@gmail.com>
2022-05-05 05:11:58 +0800

    feat: 新增登入功能的 Schema

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -7,6 +7,7 @@ module.exports = gql`
 
     type Mutation {
         register(registerInput: RegisterInput): User!
+        login(username: String!, password: String!): User!
     }
 
     input RegisterInput {
