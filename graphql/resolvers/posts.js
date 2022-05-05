790fa67149cb240427dfdfef2bde63fb27219779
leeyuljung <rita810605@gmail.com>
2022-05-06 04:58:24 +0800

    feat: 新增建立貼文的 resolver 及建立 context 轉發 request

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,6 @@
 const Post = require('../../models/Post');
+const checkAuth = require('../../utils/checkAuth');
+const { AuthenticationError } = require('apollo-server');
 
 module.exports = {
     Query: {
@@ -21,6 +23,22 @@ module.exports = {
             } catch(err) { 
                 throw new Error(err);
             }
+        }
+    },
+    Mutation: {
+        async createPost(_, { body }, context) {
+            const user = checkAuth(context);
+
+            const newPost = new Post({
+                body,
+                user: user.id,
+                username: user.username,
+                createdAt: new Date().toISOString()
+            })
+
+            const post = await newPost.save();
+
+            return post;
         }
     }
 }
