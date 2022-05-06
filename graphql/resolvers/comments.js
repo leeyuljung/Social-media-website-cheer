40ee4b68c9cc7198048634f0f2981fa440255d44
leeyuljung <rita810605@gmail.com>
2022-05-07 03:22:16 +0800

    feat: 新增建立留言的 resolver

---
+++ graphql/resolvers/comments.js
@@ -0,0 +1,31 @@
+const Post = require('../../models/Post');
+const checkAuth = require('../../utils/checkAuth');
+const { UserInputError } = require('apollo-server');
+
+module.exports = {
+    Mutation: {
+        async createComment(_, { postId, body }, context) {
+            const { username } = checkAuth(context);
+
+            if(body.trim() === ''){
+                throw new UserInputError('Empty comment', {
+                    errors: {
+                        body: 'Comment body must not be empty'
+                    }
+                })
+            }
+
+            const post = await Post.findById(postId);
+
+            if(post){
+                post.comments.unshift({
+                    body,
+                    username,
+                    createdAt: new Date().toISOString()
+                })
+                await post.save();
+                return post;
+            } else throw new UserInputError('Post not found');
+        }
+    }
+}
