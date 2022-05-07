715b34e492a363adb1ef0b9d51db4b3457c9fabb
leeyuljung <rita810605@gmail.com>
2022-05-07 21:34:05 +0800

    feat: 新增刪除留言的 Schema

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -12,6 +12,7 @@ module.exports = gql`
         createPost(body: String!): Post!
         deletePost(postId: ID!): String!
         createComment(postId: ID!, body: String!): Post!
+        deleteComment(postId: ID!, commentId: ID!): Post!
     }
 
     input RegisterInput {
