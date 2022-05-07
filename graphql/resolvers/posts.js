c60728a009567b9bd6e8df057d1f0890df174539
leeyuljung <rita810605@gmail.com>
2022-05-07 23:30:41 +0800

    feat: 新增按讚貼文的 resolver

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,6 +1,6 @@
 const Post = require('../../models/Post');
 const checkAuth = require('../../utils/checkAuth');
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
 module.exports = {
     Query: {
@@ -54,6 +54,31 @@ module.exports = {
             } catch(err) {
                 throw new Error(err);
             }
+        },
+        async likePost(_, { postId }, context) {
+            const { username } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+
+            if(post){
+                if(post.likes.find(like => like.username === username)){
+                    // 如果該貼文的按讚名單中已經有本人了，則取消讚
+    
+                    // 移除掉本人
+                    post.likes = post.likes.filter(like => like.username !== username);
+                } else {
+                    // 如果該貼文的按讚名單中沒有本人，則按讚
+    
+                    // 將本人加入
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    })
+                }
+
+                await post.save();
+                return post;
+            } else throw new UserInputError('Post not found')
         }
     }
 }
