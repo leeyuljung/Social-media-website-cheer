f2727331e465ac9888cd16547fa5eaab40701e42
leeyuljung <rita810605@gmail.com>
2022-05-11 23:31:28 +0800

    feat: 新增 Home. Login. Register. MyPosts. LikedPosts 頁面

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div className=" w-8/12 px-2 py-6 mx-4 rounded-xl border border-[#ccc] bg-white">
+            <h1>Home Page</h1>
+        </div>
+    )
+}
+
+export default Home;
---
+++ client/src/pages/MyPosts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyPosts = () => {
+    return(
+        <div className=" w-8/12 px-2 py-6 mx-4 rounded-xl border border-[#ccc] bg-white">
+            <h1>MyPosts Page</h1>
+        </div>
+    )
+}
+
+export default MyPosts;
