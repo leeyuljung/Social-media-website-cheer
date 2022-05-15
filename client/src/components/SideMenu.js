2956251d78f8a78ef1c9b630a1ce3986f42bb592
leeyuljung <rita810605@gmail.com>
2022-05-16 02:37:38 +0800

    modify: 調整 SideMenu 樣式與內容

--- client/src/components/SideMenu.js
+++ client/src/components/SideMenu.js
@@ -3,42 +3,47 @@ import { NavLink } from 'react-router-dom';
 
 const SideMenu = () => {
 	return(
-		<nav className="w-60 h-auto shadow-md bg-[#fffcf6] p-4 rounded-lg border-2 border-dashed border-[#3d405b]">
-			<ul>
-				<li>
-					<NavLink to="/" className="flex items-center text-lg font-bold py-4 px-6 h-12 mb-1 text-gray-700 rounded hover:bg-[#c8c9dc] transition duration-300 ease-in-out translate-x-0 hover:translate-x-1">
-						<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-							<path strokeLinecap="round" strokeLinejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-						</svg>
-						<span>Home</span>
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to="/addPost" className="flex items-center text-lg font-bold py-4 px-6 h-12 mb-1 text-gray-700 rounded hover:bg-[#c8c9dc] transition duration-300 ease-in-out translate-x-0 hover:translate-x-1">
-						<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-							<path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-						</svg>
-						<span>Add Post</span>
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to="/myPosts" className="flex items-center text-lg font-bold py-4 px-6 h-12 mb-1 text-gray-700 rounded hover:bg-[#c8c9dc] transition duration-300 ease-in-out translate-x-0 hover:translate-x-1">
-						<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-							<path strokeLinecap="round" strokeLinejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-						</svg>
-						<span>My Posts</span>
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to="/likedPosts" className="flex items-center text-lg font-bold py-4 px-6 h-12 mb-1 text-gray-700 rounded hover:bg-[#c8c9dc] transition duration-300 ease-in-out translate-x-0 hover:translate-x-1">
-						<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-							<path strokeLinecap="round" strokeLinejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-						</svg>
-						<span>Liked Posts</span>
-					</NavLink>
-				</li>
-			</ul>
-		</nav>
+    <nav className="w-[300px] h-fit pb-2 shadow-md bg-[#ffffff91] rounded-lg tracking-wider">
+      <div className="pt-4 pb-2 px-6">
+        <div className="flex items-center">
+          {/* 頭貼 */}
+          <div>
+            <img src="https://i.imgur.com/u2xDgkr.jpg" className="rounded-full w-20 border-[4px] border-[#fff]" alt="Avatar"/>
+          </div>
+          {/* Username */}
+          <div className="ml-3">
+            <p className="text-base font-black text-[#676a8c]">Lee Yuljung</p>
+          </div>
+        </div>
+      </div>
+      <hr className="my-2" />
+      <ul className="px-4">
+        <li>
+          <NavLink to="/" className="flex items-center text-base font-bold py-4 px-6 h-12 mb-1 text-[#676a8c] rounded hover:bg-[#c8c9dc] transition duration-300 ease-in-out translate-x-0 hover:translate-x-1">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+            </svg>
+            <span>Home</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/myPosts" className="flex items-center text-base font-bold py-4 px-6 h-12 mb-1 text-[#676a8c] rounded hover:bg-[#c8c9dc] transition duration-300 ease-in-out translate-x-0 hover:translate-x-1">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+            </svg>
+            <span>My Posts</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/cheeredPosts" className="flex items-center text-base font-bold py-4 px-6 h-12 mb-1 text-[#676a8c] rounded hover:bg-[#c8c9dc] transition duration-300 ease-in-out translate-x-0 hover:translate-x-1">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <span>Cheered Posts</span>
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
 	)
 }
 
