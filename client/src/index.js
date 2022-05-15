56a68329caf2d121a854806fb724d6bfd14f3921
leeyuljung <rita810605@gmail.com>
2022-05-16 02:40:30 +0800

    modify: 修改所有頁面樣式

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import ApolloProvider from './ApolloProvider';
 import './index.css';
+import 'react-vertical-timeline-component/style.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
