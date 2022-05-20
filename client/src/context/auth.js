c396e39927a3ed711a9f98b4d902a9282eb90410
leeyuljung <rita810605@gmail.com>
2022-05-21 03:03:35 +0800

    modify: 登入時將 JWT 存入 localstorage，登出或過期時移除 JWT

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,12 +1,23 @@
 import { createContext, useReducer } from "react";
+import jwtDecode from "jwt-decode";
 
 const initialState = {
   user: null,
 };
 
+if (localStorage.getItem("jwtToken")) {
+  const decodedToken = jwtDecode(localStorage.getItem("jwtToken"));
+
+  if (decodedToken.exp * 1000 < Date.now()) {
+    localStorage.removeItem("jwtToken");
+  } else {
+    initialState.user = decodedToken;
+  }
+}
+
 const AuthContext = createContext({
   user: null,
-  login: (userData) => {},
+  login: () => {},
   logout: () => {},
 });
 
@@ -33,6 +44,7 @@ function AuthProvider(props) {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   function login(userData) {
+    localStorage.setItem("jwtToken", userData.token);
     dispatch({
       type: "LOGIN",
       payload: userData,
@@ -40,6 +52,7 @@ function AuthProvider(props) {
   }
 
   function logout() {
+    localStorage.removeItem("jwtToken");
     dispatch({
       type: "LOGOUT",
     });
