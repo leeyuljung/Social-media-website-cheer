338a9c947fa0f6effa1c5da6d9af583d29b1d144
leeyuljung <rita810605@gmail.com>
2022-05-21 01:36:08 +0800

    modify: 調整 Header 和 SideMenu 來做會員狀態的顯示

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,34 +1,40 @@
-import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
-import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import { useContext, Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { AuthContext } from "../context/auth";
+import { Link } from "react-router-dom";
 
 const menu = [
   {
-    name: 'Home',
-    href: '#'
+    name: "Home",
+    href: "#",
   },
   {
-    name: 'Add Post',
-    href: '#'
+    name: "Add Post",
+    href: "#",
   },
   {
-    name: 'My Posts',
-    href: '#'
+    name: "My Posts",
+    href: "#",
   },
-  { 
-    name: 'Liked Posts', 
-    href: '#'
-  }
-]
+  {
+    name: "Liked Posts",
+    href: "#",
+  },
+];
 
 const Header = () => {
+  const { logout } = useContext(AuthContext);
+
   return (
     <Popover className="bg-[#3d405b]">
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="flex justify-between items-center border-gray-100 py-4 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <button>
-              <span className="text-[#ffd46f] font-bold text-xl tracking-widest">Cheer :)</span>
+              <span className="text-[#ffd46f] font-bold text-xl tracking-widest">
+                Cheer :)
+              </span>
             </button>
           </div>
           {/* 手機 Menu 按鈕 */}
@@ -39,15 +45,34 @@ const Header = () => {
             </Popover.Button>
           </div>
           <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-            <button type="button" className="px-3 pt-1 pb-1 mr-3 bg-[#dadbea] text-[#676a8c] font-bold text-sm leading-normal uppercase rounded hover:bg-[#ffd46f] hover:scale-105 active:bg-[#ffd46f] active:shadow-lg transition duration-200 ease-in-out flex align-center items-center scale-100 relative">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+            <button
+              type="button"
+              className="px-3 pt-1 pb-1 mr-3 bg-[#dadbea] text-[#676a8c] font-bold text-sm leading-normal uppercase rounded hover:bg-[#ffd46f] hover:scale-105 active:bg-[#ffd46f] active:shadow-lg transition duration-200 ease-in-out flex align-center items-center scale-100 relative"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 mr-1"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
               </svg>
               Add Post
             </button>
-            <button className="whitespace-nowrap text-base font-medium text-gray-400 hover:text-gray-300">
+
+            <Link
+              to="/login"
+              className="whitespace-nowrap text-base font-medium text-gray-400 hover:text-gray-300"
+              onClick={logout}
+            >
               Log out
-            </button>
+            </Link>
           </div>
         </div>
       </div>
@@ -99,7 +124,7 @@ const Header = () => {
         </Popover.Panel>
       </Transition> */}
     </Popover>
-  )
-}
+  );
+};
 
 export default Header;
--- client/src/components/SideMenu.js
+++ client/src/components/SideMenu.js
@@ -1,50 +1,101 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { AuthContext } from "../context/auth";
 
 const SideMenu = () => {
-	return(
+  const { user } = useContext(AuthContext);
+
+  return (
     <nav className="w-[300px] h-fit pb-2 shadow-md bg-[#ffffff91] rounded-lg tracking-wider">
       <div className="pt-4 pb-2 px-6">
         <div className="flex items-center">
           {/* 頭貼 */}
           <div>
-            <img src="https://i.imgur.com/u2xDgkr.jpg" className="rounded-full w-20 border-[4px] border-[#fff]" alt="Avatar"/>
+            <img
+              src="https://i.imgur.com/u2xDgkr.jpg"
+              className="rounded-full w-20 border-[4px] border-[#fff]"
+              alt="Avatar"
+            />
           </div>
           {/* Username */}
           <div className="ml-3">
-            <p className="text-base font-black text-[#676a8c]">Lee Yuljung</p>
+            <p className="text-base font-black text-[#676a8c]">
+              {user ? user.username : "Nobody"}
+            </p>
           </div>
         </div>
       </div>
       <hr className="my-2" />
       <ul className="px-4">
         <li>
-          <NavLink to="/" className="flex items-center text-base font-bold py-4 px-6 h-12 mb-1 text-[#676a8c] rounded hover:bg-[#c8c9dc] transition duration-300 ease-in-out translate-x-0 hover:translate-x-1">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+          <NavLink
+            to="/"
+            className="flex items-center text-base font-bold py-4 px-6 h-12 mb-1 text-[#676a8c] rounded hover:bg-[#c8c9dc] transition duration-300 ease-in-out translate-x-0 hover:translate-x-1"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 mr-2"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+              />
             </svg>
             <span>Home</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/myPosts" className="flex items-center text-base font-bold py-4 px-6 h-12 mb-1 text-[#676a8c] rounded hover:bg-[#c8c9dc] transition duration-300 ease-in-out translate-x-0 hover:translate-x-1">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+          <NavLink
+            to="/myPosts"
+            className="flex items-center text-base font-bold py-4 px-6 h-12 mb-1 text-[#676a8c] rounded hover:bg-[#c8c9dc] transition duration-300 ease-in-out translate-x-0 hover:translate-x-1"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 mr-2"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+              />
             </svg>
             <span>My Posts</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/cheeredPosts" className="flex items-center text-base font-bold py-4 px-6 h-12 mb-1 text-[#676a8c] rounded hover:bg-[#c8c9dc] transition duration-300 ease-in-out translate-x-0 hover:translate-x-1">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+          <NavLink
+            to="/cheeredPosts"
+            className="flex items-center text-base font-bold py-4 px-6 h-12 mb-1 text-[#676a8c] rounded hover:bg-[#c8c9dc] transition duration-300 ease-in-out translate-x-0 hover:translate-x-1"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 mr-2"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
             </svg>
             <span>Cheered Posts</span>
           </NavLink>
         </li>
       </ul>
     </nav>
-	)
-}
+  );
+};
 
 export default SideMenu;
