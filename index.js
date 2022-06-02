fa10c039d8ddb3d15cf324f3721e1690eadfd043
leeyuljung <rita810605@gmail.com>
2022-06-02 19:10:52 +0800

    modify: 上線前修改 port 與 scripts

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config");
 
+const PORT = process.env.PORT || 5000;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -15,8 +17,11 @@ mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
     console.log("MongoDB Connected");
-    return server.listen({ port: 5000 });
+    return server.listen({ port: PORT });
   })
   .then((res) => {
     console.log(`Server is running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.log(err);
   });
